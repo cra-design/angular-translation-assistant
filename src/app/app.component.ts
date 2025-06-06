b2ea440714d8e00dc97ca85f3064dce6266af73a
Amber <amber.leblanc@cra-arc.gc.ca>
2025-06-06 08:59:41 -0400

    Fix 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import {HeaderComponent} from './header/header.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
-import {ContentAssistantComponent} from './content-assistant/content-assistant.component';
-import {PageAssistantComponent} from './page-assistant/page-assistant.component';
-import {ImageAssistantComponent} from './image-assistant/image-assistant.component';
-import {AboutUsComponent} from './about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, HeaderComponent, SidebarComponent, ContentAssistantComponent, PageAssistantComponent, ImageAssistantComponent, AboutUsComponent],
+  imports: [RouterOutlet, RouterModule, HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ca';
+  title = 'GenAI Content Assistant';
 }
