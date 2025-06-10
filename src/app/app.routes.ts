215b40b9554d5ff4233fbe85f1446827f9dab829
RosaZheng <Rong.Zheng@cra-arc.gc.ca>
2025-06-10 10:23:11 -0400

    template create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
-import {Routes} from '@angular/router';
-import {ContentAssistantComponent} from './content-assistant/content-assistant.component';
-import {PageAssistantComponent} from './page-assistant/page-assistant.component';
-import {ImageAssistantComponent} from './image-assistant/image-assistant.component';
-import {AboutUsComponent} from './about-us/about-us.component';
+import { Routes } from '@angular/router';
+import { ContentAssistantComponent } from './content-assistant/content-assistant.component';
+import { PageAssistantComponent } from './page-assistant/page-assistant.component';
+import { ImageAssistantComponent } from './image-assistant/image-assistant.component';
+import { TranslationAssistantComponent } from './translation-assistant/translation-assistant.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
   {
@@ -19,6 +20,11 @@ export const routes: Routes = [
     path: 'image-assistant',
     component: ImageAssistantComponent,
     title: 'Image assistant',
+  },
+  {
+    path: 'translation-assistant',
+    component: TranslationAssistantComponent,
+    title: 'Translation assistant',
   },
   {
     path: 'about-us',
---
+++ src/app/translation-assistant/translation-assistant.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-translation-assistant',
+  imports: [],
+  templateUrl: './translation-assistant.component.html',
+  styleUrl: './translation-assistant.component.css'
+})
+export class TranslationAssistantComponent {
+
+}
